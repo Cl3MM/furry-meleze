6e415ffef703a9f40f852e8d0f13d83727643c2b
Clément Roullet <clement.roullet@gmail.com>
2014-11-19 14:31:21 +0100

    Added produits order

--- app/controllers/produits_controller.rb
+++ app/controllers/produits_controller.rb
@@ -6,7 +6,7 @@ class ProduitsController < ApplicationController
   # GET /produits
   # GET /produits.json
   def index
-    @produits = Produit.all
+    @produits = Produit.all.order_by(nom: 1).paginate(page: params[:page], per_page: 20)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -16,7 +16,7 @@ class ProduitsController < ApplicationController
 
   def search
     if params.has_key?(:query)
-      @produits = Produit.where(nom: /#{params[:query]}/i).paginate(page: params[:page], per_page: 20)
+      @produits = Produit.where(nom: /#{params[:query]}/i).order_by(:nom).paginate(page: params[:page], per_page: 50)
       #binding.pry
     end
     respond_to do |format|

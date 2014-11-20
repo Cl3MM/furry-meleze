c444c1f4c3e0cd1cc4e38f2d88e8342a030ef423
Clément Roullet <clement.roullet@gmail.com>
2014-11-20 18:27:58 +0100

    :joy_cat: Ajout du bon encodage dans BalanceCtrl, correction recherche produits

--- app/controllers/produits_controller.rb
+++ app/controllers/produits_controller.rb
@@ -16,7 +16,7 @@ class ProduitsController < ApplicationController
 
   def search
     if params.has_key?(:query)
-      @produits = Produit.where(nom: /#{params[:query]}/i).order_by(:nom).paginate(page: params[:page], per_page: 50)
+      @produits = Produit.where(nom: /#{params[:query]}/i).order_by(nom: 1).paginate(page: params[:page], per_page: 50)
       #binding.pry
     end
     respond_to do |format|

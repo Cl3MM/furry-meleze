83c67381b87f11610a4314cfa4eaf47ee6b7a959
Clément Roullet <clement.roullet@gmail.com>
2015-02-22 15:07:31 +0100

    Fixed level IV

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,10 +5,10 @@ module ApplicationHelper
     css_class = "fa fa-caret-#{fa_direction}"
     direction = column == find_sorted_column && sort_direction == "asc" ? "desc" : "asc"
     title_link = content_tag :span, nil do
-      "#{content_tag(:i, nil, class: fa_direction)} #{title}".html_safe
+      "#{content_tag(:i, nil, class: css_class)} #{title}".html_safe
     end
     #title = "#{content_tag(:i, "", class: fa_direction)} &nbsp;#{title}".html_safe
-    link_to title_link, {sort: column, direction: direction}, {class: css_class}
+    link_to title_link, {sort: column, direction: direction} #, {class: css_class}
     #link_to title, {sort: column, direction: direction}, {:class => css_class} do
       #content_tag :i, "", class: "fa fa-carret-down"
       #title

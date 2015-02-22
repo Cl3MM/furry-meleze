617882d7d11e4403f853045ab5785e2616fc8996
Clément Roullet <clement.roullet@gmail.com>
2015-02-22 14:47:12 +0100

    Try to fix headers font

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     css_class = "fa fa-caret-#{fa_direction}"
     direction = column == find_sorted_column && sort_direction == "asc" ? "desc" : "asc"
     title = "#{content_tag(:i, "", class: fa_direction)} &nbsp;#{title}".html_safe
-    link_to title, {sort: column, direction: direction}, {class: css_class}
+    link_to raw(title), {sort: column, direction: direction}, {class: css_class}
     #link_to title, {sort: column, direction: direction}, {:class => css_class} do
       #content_tag :i, "", class: "fa fa-carret-down"
       #title

236660c03a9796e7883ee5ccda7400186922922f
Clément Roullet <clement.roullet@gmail.com>
2015-06-03 16:33:08 +0200

    Added impossibility to delete ebsdds :en_attente :+1:

--- app/controllers/ebsdds_controller.rb
+++ app/controllers/ebsdds_controller.rb
@@ -434,7 +434,7 @@ class EbsddsController < ApplicationController
   # DELETE /ebsdds/1
   # DELETE /ebsdds/1.json
   def destroy
-    if @ebsdd.pesees.any? || @ebsdd.poids > 0
+    if @ebsdds.status == :en_attente || @ebsdd.pesees.any? || @ebsdd.poids > 0
       @ebsdd.update_attribute(:status, :deleted)
       @ebsdd.update_attribute(:deleted_at, Time.now)
     else

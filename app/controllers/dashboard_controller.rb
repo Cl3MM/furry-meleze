cc0b3fe96456b2a2bb1bd16b36be281ba02da36c
Clément Roullet <clement.roullet@gmail.com>
2015-06-14 14:29:22 +0200

    Ajout des 10 derniers BSD créés

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,11 +2,12 @@ class DashboardController < ApplicationController
   before_filter :authenticate_utilisateur!
 
   def index
-    @nouveaux  = Ebsdd.where(status: :nouveau).limit(10)
+    #@nouveaux  = Ebsdd.where(status: :nouveau).limit(10)
+    @bons        = BonDeSortie.desc(:id).limit(10)
     @en_attente  = Ebsdd.where(status: :en_attente).limit(10)
-    @incomplete  = Ebsdd.where(status: :incomplet).limit(10)
-    @complete    = Ebsdd.where(status: :complet).limit(10)
-    @attachments = Attachment.all.limit(5)
+    #@incomplete  = Ebsdd.where(status: :incomplet).limit(10)
+    #@complete    = Ebsdd.where(status: :complet).limit(10)
+    #@attachments = Attachment.all.limit(5)
     all_alertes  = Ebsdd.seuils
     @max_alertes = all_alertes.count
     @alertes     = all_alertes.map { |al| al if al[:poids] > al[:seuil] }.compact.take(10)
